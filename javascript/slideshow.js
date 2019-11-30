810b1385672e4793cecf292e02a549458d144c55
kristiinakoster <kosterkristiina@gmail.com>
2019-12-01 01:13:51 +0200

    teenuste lisamine

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,5 +1,5 @@
 
-var slideIndex = 1;
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -8,9 +8,11 @@ function plusSlides(n) {
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
-}
+}*/
 
-function showSlides(n) {
+
+
+/*function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
@@ -25,5 +27,26 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active"
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  /*captionText.innerHTML = dots[slideIndex-1].alt;
+
+}*/
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  C
 }
