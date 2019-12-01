e63a136301519f1b76a52484028c7aa65a186f35
kristiinakoster <kosterkristiina@gmail.com>
2019-12-01 03:52:15 +0200

    parandused

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,45 +1,6 @@
 var slideIndex = 0;
 showSlides();
 
-<<<<<<< HEAD
-/*var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}*/
-
-
-
-/*function showSlides(n) {
-=======
-function showSlides() {
->>>>>>> b50e1e4189f4e2bb55c643023de122bc924486fa
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-<<<<<<< HEAD
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active"
-  /*captionText.innerHTML = dots[slideIndex-1].alt;
-
-}*/
-
-var slideIndex = 0;
-showSlides();
-
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -52,13 +13,7 @@ function showSlides() {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  C
-}
-=======
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
->>>>>>> b50e1e4189f4e2bb55c643023de122bc924486fa
